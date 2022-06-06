5a2604306d8e6f9954891453ed66427e863c816e
Midhul <midhul.v@gmail.com>
2022-06-06 02:14:05 -0400

    MLC and PCM runners

--- mio/app.py
+++ mio/app.py
@@ -13,7 +13,10 @@ def main(args=[]):
     mlc = MLCRunner(env.get_mlc_path())
     mlc.init(os.path.join(env.get_stats_path(), 'foo.mlc.txt'), [0], 0, {})
 
+    pcm_mem = PcmMemoryRunner(env.get_pcm_path())
+
     mlc.run(10)
+    pcm_mem.run(os.path.join(env.get_stats_path(), 'foo.pcm-memory.txt'), 5)
     mlc.wait()
 
     print('Complete')
