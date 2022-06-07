9694ba0ff9a7d134a4510e760de36aa5410fe9fc
Midhul <midhul.v@gmail.com>
2022-06-07 07:59:49 -0400

    run fio

--- run_fio.sh
+++ run_fio.sh
@@ -17,7 +17,7 @@ numactl --membind 3 $FIO_PATH/fio --cpus_allowed=8,12 --filename=/dev/nvme1n1 --
 pids+=($!);
 numactl --membind 3 $FIO_PATH/fio --cpus_allowed=16,20 --filename=/dev/nvme4n1 --name test  --ioengine=libaio  --direct=1  --rw=$workload  --gtod_reduce=0  --cpus_allowed_policy=split  --time_based  --size=1G  --runtime=$duration  --numjobs=2  --bs=$io_size  --iodepth=$io_depth --group_reporting > $STATS_PATH/$config.fio2.txt 2>&1 &
 pids+=($!);
-numactl --membind 3 $FIO_PATH/fio --cpus_allowed=24,32 --filename=/dev/nvme5n1 --name test  --ioengine=libaio  --direct=1  --rw=$workload  --gtod_reduce=0  --cpus_allowed_policy=split  --time_based  --size=1G  --runtime=$duration  --numjobs=2  --bs=$io_size  --iodepth=$io_depth --group_reporting > $STATS_PATH/$config.fio3.txt 2>&1 &
+numactl --membind 3 $FIO_PATH/fio --cpus_allowed=24,28 --filename=/dev/nvme5n1 --name test  --ioengine=libaio  --direct=1  --rw=$workload  --gtod_reduce=0  --cpus_allowed_policy=split  --time_based  --size=1G  --runtime=$duration  --numjobs=2  --bs=$io_size  --iodepth=$io_depth --group_reporting > $STATS_PATH/$config.fio3.txt 2>&1 &
 pids+=($!);
 
 wait "${pids[@]}"
