f7cf6e1e0a1ce13b9bc7ded298d05e93969b0c29
Saksham Agarwal <sakshamagarwals@live.com>
2022-06-09 11:18:18 -0400

    Update pcm.py

--- mio/pcm.py
+++ mio/pcm.py
@@ -49,6 +49,29 @@ class PcmMemoryRunner:
     def cleanup(self):
         if self.proc:
             self.proc.kill()
+            
+            
+class PcmLatencyRunner:
+    def __init__(self, pcm_path):
+        self.pcm_memory_path = os.path.join(pcm_path, 'pcm-latency.x')
+
+        self.proc = None
+
+    # Run for a given duration (blocking call)
+    def run(self, out_path, duration):
+        out_f = open(out_path, 'w')
+        args = [self.pcm_memory_path]
+
+        self.proc = subprocess.Popen(args, stdout=out_f, stderr=subprocess.STDOUT)
+        try:
+            self.proc.wait(timeout=duration)
+        except subprocess.TimeoutExpired:
+            self.proc.terminate()
+            self.proc = None
+
+    def cleanup(self):
+        if self.proc:
+            self.proc.kill()
 
     
 
